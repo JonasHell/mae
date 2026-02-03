e9629f296520a7f68af7e89cd663a4b7c93ce201
JonasHell <jonas.hellgoth@gmx.de>
2026-02-03 11:12:42 +0100

    try shallow model

--- my_models_mae.py
+++ my_models_mae.py
@@ -317,6 +317,22 @@ def mae_vit_base_patch16_small_(**kwargs):
     return model
 
 
+def mae_vit_base_patch16_shallow_(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16,
+        embed_dim=768,
+        depth=6,
+        num_heads=12,
+        decoder_embed_dim=512,
+        decoder_depth=4,
+        decoder_num_heads=16,
+        mlp_ratio=4,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        **kwargs
+    )
+    return model
+
+
 def mae_vit_base_patch16_maester_(**kwargs):
     model = MaskedAutoencoderViT(
         patch_size=16,
@@ -421,8 +437,8 @@ mae_vit_base_patch16 = mae_vit_base_patch16_dec512d8b  # decoder: 512 dim, 8 blo
 mae_vit_large_patch16 = mae_vit_large_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_huge_patch14 = mae_vit_huge_patch14_dec512d8b  # decoder: 512 dim, 8 blocks
 
-mae_vit_base_patch16_small = mae_vit_base_patch16_small_  # decoder: 512 dim, 8 blocks
-
+mae_vit_base_patch16_small = mae_vit_base_patch16_small_
+mae_vit_base_patch16_shallow = mae_vit_base_patch16_shallow_
 
 mae_vit_base_patch16_maester = mae_vit_base_patch16_maester_
 
